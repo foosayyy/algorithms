4c739c0d96a0ad3203f263093ba4de59d72a1e2f
foosayyy <85921796+foosayyy@users.noreply.github.com>
2021-10-27 20:58:15 -0700

    Create vector Insertion sort.cpp

---
+++ week2/error/vector Insertion sort.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void insertionsort(vector<int>a, int n){
+	for(int i=1; i<a.size()-1; i++){
+		int value = a[i];
+		int hole=i;
+		while(hole > 0 && a[hole-1] > value){
+			a[hole-1] = a[hole];
+			hole = hole-1;
+		}
+		value = a[hole];
+	}
+}
+
+void sorted(vector<int>a, int n){
+	for(int i=0; i<a.size(); i++){
+		cout<<a[i] << " ";
+	}
+	cout<<endl;
+}
+
+int main(){
+	int n;
+	cin>>n;
+	vector<int> a;
+	for(int i=0; i<n; i++){
+		int x;
+		cin>>x;
+		a.push_back(x);
+	}
+	insertionsort(a,n);
+	cout<<"Sorted Array: ";
+	sorted(a,n);
+
+	return 0;
+}
