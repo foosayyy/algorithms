6e983996bccce2959ec4519dc7cd2afd2212cc79
foosayyy <85921796+foosayyy@users.noreply.github.com>
2021-10-27 23:35:00 -0700

    Create InsertionSort.cpp

---
+++ week2/InsertionSort.cpp
@@ -0,0 +1,45 @@
+/*
+INPUT 
+7
+64
+34
+25
+12
+22
+11
+90
+****************************************
+OUTPUT
+Sorted Array: 11 12 22 25 34 64 90 
+****************************************
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+void insertionsort(vector<int> a, int n){
+   for(int i=1; i<n; i++){
+      int value = a[i];
+      int hole = i-1;
+      while(hole >= 0 && a[hole] >= value){
+         a[hole+1] = a[hole];
+         hole = hole-1;
+      }
+      a[hole+1] = value;
+   }
+   cout<<"Sorted Array: ";
+   for(int i=0; i<n; i++){
+      cout<<a[i] << " ";
+   }
+}
+
+int main(){
+   int n;
+   cin>>n;
+   vector<int> a;
+   for(int i= 0; i< n; i++){
+      int x;
+      cin>>x;
+      a.push_back(x);
+   }
+   insertionsort(a,n);
+}
